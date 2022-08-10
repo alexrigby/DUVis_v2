236847113436b0736470bae3d6a1456d384dde51
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-08-10 16:22:25 +0100

    parse dwr usice data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+
+// import data from "./data";
 
 import Header from "./components/header/Header";
 import SideBar from "./components/sideBar/SideBar";
 import Cytoscape from "./components/cytoscape/Cytoscape";
 
+import data from "./data";
+import resetVeiwOnDoubleClick from "./AppFunctions/resetveiwOnDoubleClick";
+import makeCyElements from "./functions/makeCyElements";
+
+import dataset from "./data/TDR Matrix_Subset.txt";
+import links from "./data/links.txt";
+
+// const dataw = await parseData("../data/TDR Matrix_Subset.txt");
+// console.log(dataw);
+
 export function App() {
-  //sets initial state for clicked node
-  const [selectedNode, setselectedNode] = useState({ id: "" });
+  //sets state of cy
+  const [cyState, setCyState] = useState({
+    w: window.innerWidth,
+    h: window.innerHeight,
+    cy: null,
+    elements: CytoscapeComponent.normalizeElements(data),
+  });
+
+  //sets initial state for selected node
+  const [selectedNode, setSelectedNode] = useState({ id: "" });
+
+  useEffect(() => {
+    async function parseAllData() {
+      const cyElms = await makeCyElements(dataset, links);
+      console.log(cyElms);
+    }
+    parseAllData();
+  }, []);
 
   return (
-    <div className="container" onDoubleClick={() => setselectedNode({ id: "" })}>
-      <Header />
+    <div className="container" onDoubleClick={() => resetVeiwOnDoubleClick(setSelectedNode, cyState)}>
+      <Cytoscape
+        cyState={cyState}
+        setCyState={setCyState}
+        setSelectedNode={setSelectedNode}
+        selectedNode={selectedNode}
+      />
+      <Header cyState={cyState} />
       <SideBar selectedNode={selectedNode} /> {/* pass state as prop to Side Bar*/}
-      <Cytoscape setselectedNode={setselectedNode} />
     </div>
   );
 }
