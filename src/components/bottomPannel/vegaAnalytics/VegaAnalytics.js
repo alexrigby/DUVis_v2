fd3538555a377c7846a546e231b917d917308820
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-08-23 17:09:36 +0100

    paresd data for vega

--- src/components/bottomPannel/vegaAnalytics/VegaAnalytics.js
+++ src/components/bottomPannel/vegaAnalytics/VegaAnalytics.js
@@ -11,21 +11,26 @@ export function VegaAnalytics({ selectedBottomVis, vegaAnalyticsData }) {
   if (vegaAnalyticsData.current.actData !== null) {
     //unique category names
     const categorys = [...new Set(actData.map((act) => act["Activity Category"]))];
-
+    //get groups of each activities in category
     const activityByCategory = categorys.map((category) =>
       actData.filter((act) => act["Activity Category"] === category)
     );
-    console.log(activityByCategory);
-    const categoryPlotData = activityByCategory.map((catAct) => ({
-      values: dates.map((date) => ({
-        cat: catAct.filter(
-          (act) => new Date(date.date) >= new Date(act.startDate) && new Date(date.date) <= new Date(act.endDate)
+    //add Activity count per month to date array
+    const categoryPerDate = activityByCategory.map((catAct) =>
+      dates.map((date) => ({
+        [catAct[0]["Activity Category"]]: catAct.filter(
+          (act) => new Date(date.date) >= new Date(act.startDate) && new Date(date.date) < new Date(act.endDate)
         ).length,
         ...date,
-      })),
-    }));
+      }))
+    );
 
-    console.log(categoryPlotData);
+    //flatten array or arrays and merge based on date collumn
+    let plotData = {};
+    categoryPerDate.flat().forEach((a) => (plotData[a.date] = { ...plotData[a.date], ...a }));
+    plotData = Object.values(plotData);
+
+    console.log({ values: plotData });
 
     const pub = actData.filter((pub) => pub["Activity Category"] === "Publication");
     const pubCount = {
@@ -43,7 +48,7 @@ export function VegaAnalytics({ selectedBottomVis, vegaAnalyticsData }) {
       mark: { type: "line", color: "#0084FF", interpolate: "monotone" },
       encoding: {
         x: { timeUnit: "yearmonthdate", field: "date", title: "date", type: "temporal", axis: { title: "Date" } },
-        y: { field: "pub", type: "quantitative", axis: { title: "quantity" } },
+        y: { field: { repeat: "layer" }, type: "quantitative", axis: { title: "quantity" } },
       },
       data: { name: "values" }, // note: vega-lite data attribute is a plain object instead of an array
     };
@@ -51,11 +56,11 @@ export function VegaAnalytics({ selectedBottomVis, vegaAnalyticsData }) {
     if (selectedBottomVis === "vegaAnalyticsButton") {
       return (
         <div className="vegaAnalytics">
-          <VegaLite spec={spec} data={pubCount} />
+          <VegaLite spec={spec} data={{ values: plotData }} />
         </div>
       );
     }
   }
 }
-
+// repeat={{ layer: [categorys.map((c) => c)] }}
 export default VegaAnalytics;
