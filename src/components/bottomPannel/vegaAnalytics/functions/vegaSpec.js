aafccab418a5754fc2e2d07d4a0e1b2306de6f90
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-08-25 13:45:32 +0100

    got legendSelect Param to work

--- src/components/bottomPannel/vegaAnalytics/functions/vegaSpec.js
+++ src/components/bottomPannel/vegaAnalytics/functions/vegaSpec.js
@@ -3,47 +3,46 @@ import parseVegaData from "./parseVegaData";
 export function vegaSpec(actData, dates) {
   const { vegaData, categorys } = parseVegaData(actData, dates);
 
-  const timeSeriesLayers = categorys.map((cat, i) => ({
-    mark: { type: "line", strokeWidth: 3, interpolate: "monotone" },
+  const spec = {
+    padding: 20,
+    data: { name: "vegaData" },
     params: [
       {
-        name: `param-${i}`,
+        name: "legendSelect",
         select: { type: "point", fields: ["category"] },
         bind: "legend",
       },
     ],
-    encoding: {
-      x: { timeUnit: "yearmonth", field: "date", title: "date", type: "temporal", axis: { title: "Date" } },
-      y: { field: cat, type: "quantitative", axis: { title: "", tickMinStep: 1 } },
-      color: {
-        field: "category",
-        datum: cat,
-      },
-      opacity: {
-        condition: { param: `param-${i}`, value: 1 },
-        value: 0.2,
-      },
-    },
-  }));
-
-  const spec = {
-    // width: "container",
-    // height: "container",
-    padding: 20,
-    data: { name: "vegaData" },
     vconcat: [
       {
         width: "container",
-        layer: timeSeriesLayers,
+        transform: [{ fold: categorys, as: ["category", "y"] }], //adds all relivent input feilds to one layer
+        mark: { type: "area", strokeWidth: 3, interpolate: "monotone" },
+        encoding: {
+          x: { timeUnit: "yearmonth", field: "date", title: "date", type: "temporal", axis: { title: "Date" } },
+          y: { field: "y", type: "quantitative", axis: { title: "", tickMinStep: 1 } },
+          color: {
+            field: "category",
+            type: "nominal",
+          },
+          opacity: {
+            condition: { param: "legendSelect", value: 1 },
+            value: 0.2,
+          },
+        },
       },
       {
         width: "container",
         mark: "bar",
         encoding: {
-          x: { field: "category", type: "ordinal" },
-          y: { field: "count", type: "quantitative" },
+          x: { field: "category", type: "ordinal", axis: { labelAngle: 0 } },
+          y: { field: "count", type: "quantitative", axis: { title: "" } },
           color: {
             field: "category",
+          },
+          opacity: {
+            condition: { param: `legendSelect`, value: 1 },
+            value: 0.2,
           },
         },
       },
@@ -69,3 +68,28 @@ export function vegaSpec(actData, dates) {
 }
 
 export default vegaSpec;
+
+//PREVIOUSLY MADE LAYERS FOR EACH ITEM< NOW I HAVE USED "transfor: fold" ----- ALLOWS PARAM BINDING
+
+// const timeSeriesLayers = categorys.map((cat, i) => ({
+//   mark: { type: "line", strokeWidth: 3, interpolate: "monotone" },
+//   params: [
+//     {
+//       name: `param-${i}`,
+//       select: { type: "point", fields: ["category"] },
+//       bind: "legend",
+//     },
+//   ],
+//   encoding: {
+//     x: { timeUnit: "yearmonth", field: "date", title: "date", type: "temporal", axis: { title: "Date" } },
+//     y: { field: cat, type: "quantitative", axis: { title: "", tickMinStep: 1 } },
+//     color: {
+//       field: "category",
+//       datum: cat,
+//     },
+//     opacity: {
+//       condition: { param: `param-${i}`, value: 1 },
+//       value: 0.2,
+//     },
+//   },
+// }));
