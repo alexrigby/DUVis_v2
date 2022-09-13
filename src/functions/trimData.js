b0b8da40d0f1179b1416b2fc65fc22db27fc1ea8
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-09-13 12:44:16 +0100

    changed trimmdata function

--- src/functions/trimData.js
+++ src/functions/trimData.js
@@ -1,15 +1,19 @@
 export function trimData(actData, prPeriod, storyIds) {
+  //if no filter then return whole dataset
   if (storyIds === null && prPeriod.pr === null) {
     return actData;
+    //if story is selected but no pr return whole story
+  } else if (storyIds !== null && prPeriod.pr === null) {
+    return filterStoryData(actData, storyIds);
   }
-  //if no stroy is selected trm full dataset
-  else if (storyIds === null) {
+  //if no story is selected but a pr is selected then return progress report
+  else if (storyIds === null && prPeriod.pr !== null) {
     if (prPeriod.undefined === true) {
       return actData.filter((act) => act.startPrPeriod <= prPeriod.pr || act.startPrPeriod === "undefined");
     } else {
       return actData.filter((act) => act.startPrPeriod <= prPeriod.pr);
     }
-    //if story is selected then flter the ids of the story
+    //if story and progress report are seleted return both
   } else {
     let story = filterStoryData(actData, storyIds);
     if (prPeriod.undefined === true) {
