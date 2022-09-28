982cad4380faf1b21fe978a3cfa63b94cdd523e9
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-09-28 11:49:48 +0100

    mesing about with layout

--- src/components/cytoscape/functions/cyLayouts.js
+++ src/components/cytoscape/functions/cyLayouts.js
@@ -14,20 +14,38 @@ export const LAYOUTS = {
     animationDuration: 1000,
     nodeDimensionsIncludeLabels: true,
     // refresh: 400,
-    nodeRepulsion: (node) => 10000,
-    idealEdgeLength: (edge) => 250,
+    nodeRepulsion: function (node) {
+      return 4000;
+    },
+    idealEdgeLength: function (edge) {
+      return 300;
+    },
     randomize: false,
     fit: true,
+    gravity: 3,
+    numIter: 100000,
+    initialTemp: 1000,
+    coolingFactor: 0.99,
+    // nestingFactor: 0.3, //seems to make each wp more indipendant i.e. larger nodes float further away from center of graph
   },
   //run when chnage layout button is clicked, creates more 'drastic' layout chnage
   FCOSERandom: {
     name: "fcose",
     animationDuration: 1000,
     nodeDimensionsIncludeLabels: true,
-    nodeRepulsion: (node) => 10000,
-    idealEdgeLength: (edge) => 250,
+    nodeRepulsion: function (node) {
+      return 4000;
+    },
+    idealEdgeLength: function (edge) {
+      return 300;
+    },
     randomize: true,
     fit: true,
+    gravity: 3,
+    numIter: 100000,
+    initialTemp: 1000,
+    coolingFactor: 0.99,
+    // nestingFactor: 0.3,
   },
 };
 
