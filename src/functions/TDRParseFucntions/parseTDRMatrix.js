beb05eadfef55bb04639f2497562e5913d1e1f5c
alexrigby <76942259+alexrigby@users.noreply.github.com>
2022-10-31 13:38:44 +0000

    parseing stakeholders

--- src/functions/TDRParseFucntions/parseTDRMatrix.js
+++ src/functions/TDRParseFucntions/parseTDRMatrix.js
@@ -5,7 +5,10 @@ export async function parseTDRMatrix(url) {
   const stakeholders = data.slice(4, data.length - 1).map((sh) => sh.slice(0, 2)); // removes headers and gets 2 first collumns, tdr id and stakeholder name
   const stakeholderIDs = stakeholders.flatMap((s) => s.slice(0, 1)); // gets array of stakeholder IDs
   console.log(data);
-  const activityIDs = data.slice(3, 4);
+  const activityIDs = data
+    .slice(3, 4)
+    .flat()
+    .slice(3, data.length - 1);
   console.log(activityIDs);
 }
 
